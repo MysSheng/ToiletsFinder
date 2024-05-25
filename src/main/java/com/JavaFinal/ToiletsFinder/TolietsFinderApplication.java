8133648ff61d8b0279a3408dd0b6c5d8f1263aef
MysSheng <liuzongzhe6705@gmail.com>
2024-05-25 12:00:34 +0800

    Initial commit

---
+++ src/main/java/com/JavaFinal/ToiletsFinder/TolietsFinderApplication.java
@@ -0,0 +1,13 @@
+package com.JavaFinal.ToiletsFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolietsFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolietsFinderApplication.class, args);
+	}
+
+}
